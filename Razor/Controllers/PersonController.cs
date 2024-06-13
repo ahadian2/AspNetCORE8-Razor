de30231ca0e49f81977725f5fab99a09e5f8949f
Mohammadreza <ahadian2@gmail.com>
2024-06-13 21:42:06 +0330

    Add project files.

---
+++ Razor/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Users> users = new List<Users>() {
+              new Users { Id=1,Name="Mohammadreza",Family="Ahadian"},
+              new Users { Id=2,Name="Ahmadreza",Family="Jafari"},
+              new Users { Id=3,Name="Alireza",Family="Akbari"},
+              new Users { Id=4,Name="Hoda",Family="Mahmodi"},
+              new Users { Id=5,Name="Tanaz",Family="Rezaei"}
+            };
+            return View(users);
+        }
+        public IActionResult info()
+        {
+            Users users = new Users()
+            {
+                Id = 1,
+                Name = "Mohammadreza",
+                Family = "Ahadian"
+            };
+
+            return View(users);
+        }
+    }
+}
---
+++ Razor/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Razor.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Family { get; set; }
+    }
+}
